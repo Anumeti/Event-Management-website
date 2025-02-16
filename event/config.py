325afc981f8d9fe257d6454cbae0fdffe901523e
Aniket Meti <metianiket@gmail.com>
2025-02-16 15:18:18 +0530

    Delete event directory

--- event/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_super_secret_key'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
